92efeff81f481ed7ecd73855fa1d25e340e4f658
Devsodin <devsodin@gmail.com>
2017-09-29 22:38:03 +0200

    Dijkstra 'Working'

--- Salesman/Dijkstra.cpp
+++ Salesman/Dijkstra.cpp
@@ -6,37 +6,43 @@
 // Dijkstra ====================================================================
 // =============================================================================
 
+
+double CalcDistances(CVertex source, CVertex destination) {
+	double dist = hypot(destination.m_Point.m_X - source.m_Point.m_X, destination.m_Point.m_Y - source.m_Point.m_Y);
+	return dist;
+}
+
 void CGraph::Dijkstra(CVertex *pStart)
 {
-
-	list<CVertex*> unvisited;
-
-	for (CVertex v : m_Vertices) {
+	for (CVertex &v : m_Vertices) {
 		v.m_DijkstraDistance = numeric_limits<double>::max();
 		v.m_DijkstraVisit = false;
-		unvisited.push_front(&v);	
 	}
-
-
-	pStart->m_DijkstraDistance = 0;
-
-	CVertex* v = pStart;
 	
-	while (!unvisited.empty()) {
-		CVertex* u = unvisited.front();
-		for (CVertex* a:u->m_Neighbords) {
-			double dist = (&v->m_Point - &u->m_Point);
-			if (dist < a->m_DijkstraDistance) {
-				a->m_DijkstraDistance = dist;
-				a->m_pDijkstraPrevious = v;
+	CVertex* selected = pStart;
+	selected->m_DijkstraDistance = 0;
+	
+	while (!selected->m_DijkstraVisit) {
+		selected->m_DijkstraVisit = true;
+		
+		for (CVertex* c : selected->m_Neighbords) {
+			double dist = (c->m_Point - selected->m_Point).Module();
+			if (dist < c->m_DijkstraDistance) {
+				c->m_DijkstraDistance = dist;
+				c->m_pDijkstraPrevious = selected;
 			}
 		}
-		unvisited.pop_front();
+		
+		for (CVertex &c :m_Vertices) {
+			if (!(c.m_DijkstraVisit))
+				selected = &c;
+		}
 	}
-
 
 
 }
+
+
 
 // =============================================================================
 // DijkstraQueue ===============================================================
@@ -44,14 +50,15 @@ void CGraph::Dijkstra(CVertex *pStart)
 
 void CGraph::DijkstraQueue(CVertex *pStart)
 {
-	pStart->m_DijkstraDistance = 0;
-
-
-	for (CVertex &v : m_Vertices){
+	for (CVertex &v : m_Vertices) {
 		v.m_DijkstraDistance = numeric_limits<double>::max();
+		v.m_DijkstraVisit = false;
 	}
 
-	pStart->m_DijkstraDistance = 0;
+	CVertex* selected = pStart;
+	selected->m_DijkstraDistance = 0;
+
+
 	
 
 }
