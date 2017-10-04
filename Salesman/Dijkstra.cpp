f5f3908dc31fca142dacc9edf34800a16ccaf3e9
Devsodin <devsodin@gmail.com>
2017-10-04 14:38:25 +0200

    Dijkstra working (need to fix infinite loop)

--- Salesman/Dijkstra.cpp
+++ Salesman/Dijkstra.cpp
@@ -5,60 +5,81 @@
 // =============================================================================
 // Dijkstra ====================================================================
 // =============================================================================
-
-
-double CalcDistances(CVertex source, CVertex destination) {
-	double dist = hypot(destination.m_Point.m_X - source.m_Point.m_X, destination.m_Point.m_Y - source.m_Point.m_Y);
-	return dist;
-}
-
 void CGraph::Dijkstra(CVertex *pStart)
 {
+	// Set all vertex to infinite distance(unknown) and not visited
 	for (CVertex &v : m_Vertices) {
 		v.m_DijkstraDistance = numeric_limits<double>::max();
 		v.m_DijkstraVisit = false;
 	}
-	
+	printf("set");
+	// Set source vertex to dist 0
 	CVertex* selected = pStart;
 	selected->m_DijkstraDistance = 0;
-	
-	while (!selected->m_DijkstraVisit) {
-		selected->m_DijkstraVisit = true;
-		
-		for (CVertex* c : selected->m_Neighbords) {
-			double dist = (c->m_Point - selected->m_Point).Module();
-			if (dist < c->m_DijkstraDistance) {
-				c->m_DijkstraDistance = dist;
-				c->m_pDijkstraPrevious = selected;
+	//selected->m_DijkstraVisit = false;
+	printf("first");
+
+	while (true) {
+		printf("loop_start");
+		for (CVertex* x : selected->m_Neighbords) {
+			double dist = selected->m_DijkstraDistance + (x->m_Point - selected->m_Point).Module();
+			if (dist < x->m_DijkstraDistance) {
+				x->m_DijkstraDistance = dist;
+				x->m_pDijkstraPrevious = selected;
 			}
 		}
-		
-		for (CVertex &c :m_Vertices) {
-			if (!(c.m_DijkstraVisit))
-				selected = &c;
+		printf("neighs");
+		selected->m_DijkstraVisit = true;
+
+		CVertex* tmp = NULL;
+		for (CVertex &a : m_Vertices) {
+			if (!a.m_DijkstraVisit && (tmp == NULL|| a.m_DijkstraDistance < tmp->m_DijkstraDistance)) {
+				tmp = &a;
+				//mindist = a.m_DijkstraDistance;
+			}
+
 		}
+
+		if (tmp == NULL) {
+			break;
+		}
+		selected = tmp;
 	}
-
-
 }
+//=============================================================================
+//DijkstraQueue ===============================================================
+//=============================================================================
 
-
-
-// =============================================================================
-// DijkstraQueue ===============================================================
-// =============================================================================
 
 void CGraph::DijkstraQueue(CVertex *pStart)
-{
+{	// Set all vertex to infinite distance(unknown) and not visited
 	for (CVertex &v : m_Vertices) {
 		v.m_DijkstraDistance = numeric_limits<double>::max();
 		v.m_DijkstraVisit = false;
 	}
-
-	CVertex* selected = pStart;
-	selected->m_DijkstraDistance = 0;
-
-
 	
+	
+	CVertex s = *pStart;
+	s.m_DijkstraVisit = true;
+	s.m_DijkstraDistance = 0;
 
+	auto comparepq = [](CVertex z, CVertex x) { return z.m_DijkstraDistance < x.m_DijkstraDistance; };
+	priority_queue < CVertex, vector<CVertex>, decltype(comparepq) > pq(comparepq);
+
+	pq.push(s);
+
+	while (!pq.empty()) {
+		CVertex selected = pq.top();
+		pq.pop();
+		for (CVertex* u : selected.m_Neighbords) {
+			double dist = selected.m_DijkstraDistance + (u->m_Point - selected.m_Point).Module();
+			if (dist < u->m_DijkstraDistance) {
+				u->m_DijkstraDistance = dist;
+			}
+			if (!u->m_DijkstraVisit) {
+				u->m_DijkstraVisit = true;
+				pq.push(*u);
+			}
+		}
+	}
 }
