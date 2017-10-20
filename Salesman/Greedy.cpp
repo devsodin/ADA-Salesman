a7b33cc0d9eb865762b2d2caec1f6314bd6169b2
Devsodin <devsodin@gmail.com>
2017-10-20 20:46:07 +0200

    Greedy Working - P2 Finished

--- Salesman/Greedy.cpp
+++ Salesman/Greedy.cpp
@@ -2,41 +2,48 @@
 #include "CGraph.h"
 
 // SalesmanTrackGreedy =========================================================
+void reconstruct(CVertex* next, CTrack* track) {
+	if (track->m_Vertices.back() == next->m_pDijkstraPrevious) {
+		track->AddLast(next);
+		return;
+	}
+	else {
+		reconstruct(next->m_pDijkstraPrevious, track);
+		track->AddLast(next);
+	}
+}
 
-CTrack CGraph::SalesmanTrackGreedy(CVisits &visits) 
+CTrack CGraph::SalesmanTrackGreedy(CVisits &visits)
 {
 	CTrack track(this);
-	list<CVertex*> unvisited = visits.m_Vertices;
-	for (CVertex* v : visits.m_Vertices) {
-		v->m_VertexToVisit = true;
-		unvisited.push_back(v);
-	}
+	double minDist;
 
 	CVertex* selected = visits.m_Vertices.front();
 	track.AddFirst(selected);
-	unvisited.remove(selected);
+	visits.m_Vertices.pop_front();
 
-	do {
-		if(selected->m_VertexToVisit){
-			selected->m_VertexToVisit = false;
-			visits.m_pGraph->Dijkstra(selected);
-			double mindist = numeric_limits<double>::max();
-			for (CVertex* neigh : selected->m_Neighbords) {
-				if (neigh != visits.m_Vertices.back() && neigh->m_DijkstraDistance < mindist) {
-					if (neigh->m_VertexToVisit) {
-						selected = neigh;
-					}
-				}
+	CVertex* last = visits.m_Vertices.back();
+	visits.m_Vertices.pop_back();
+	
+	CGraph::Dijkstra(selected);
+	//Por cada nodo a visitrar
+	while (!visits.m_Vertices.empty()) {
+		//Seleccionar la proxima visita mas cercana
+		minDist = numeric_limits<double>::max();
+		for (CVertex* x : visits.m_Vertices) {
+			if (minDist > x->m_DijkstraDistance) {
+				minDist = x->m_DijkstraDistance;
+				selected = x;
 			}
-			unvisited.remove(selected);
-			
 		}
-		track.AddLast(selected);
-		
-
-	} while (unvisited.size() > 1);
-	track.AddLast(visits.m_Vertices.back());
-
-
+		//quitar de la lista y hacer camino del nodo anterior al nuevo
+		visits.m_Vertices.remove(selected);
+		reconstruct(selected, &track);
+		CGraph::Dijkstra(selected);
+	}
+	//reconstruir camino hasta el ultimo nodo
+	reconstruct(last, &track);
 	return track;
+
 }
+
